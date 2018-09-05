179ef4cfc506b6a691c965837747eed3894cf32f
Tyrone Meijn <t.meijn@thesio.eu>
2018-09-05 13:48:49 +0200

    Adds "Deep Dive" practice project to repo.

---
+++ deep_dive/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElemtComponent } from './server-elemt/server-elemt.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CockpitComponent,
+    ServerElemtComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ deep_dive/src/app/server-elemt/server-elemt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-elemt',
+  templateUrl: './server-elemt.component.html',
+  styleUrls: ['./server-elemt.component.css']
+})
+export class ServerElemtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
